9ecb7b1f8e02a7a10b2a5e607dace5e1af494cce
AtaTemel <147736343+AtaTemel@users.noreply.github.com>
2024-12-15 18:10:26 +0100

    First Trial

--- main.cpp
+++ main.cpp
@@ -12,7 +12,3 @@ void ShowMenu() {
     cout << endl <<"5-Mark Task ";
     cout << endl <<"Choose one option "<<endl;
 }
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
