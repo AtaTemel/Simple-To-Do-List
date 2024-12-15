40967aa665bc925d85fd82c4432f353b65c02767
AtaTemel <147736343+AtaTemel@users.noreply.github.com>
2024-12-15 18:06:58 +0100

    First Trial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-
-
+#include <iostream>
+using namespace std;
+void ShowMenu();
+int main() {
+ShowMenu();
+}
+void ShowMenu() {
+    cout << endl <<"1-To-Do List Manager";
+    cout << endl <<"2-Add New Task";
+    cout << endl <<"3-Edit Task";
+    cout << endl <<"4-Delete Task";
+    cout << endl <<"5-Mark Task ";
+    cout << endl <<"Choose one option "<<endl;
+}
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
